3f4eac74fcd0a5db676f736bd53887095dad5d6b
pedigru3 <contosdeferreira@hotmail.com>
2024-07-30 23:00:17 -0300

    config cors

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -23,6 +23,10 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('api', app, document)
 
+  app.enableCors({
+    origin: 'https://journey-folder.vercel.app/',
+  })
+
   await app.listen(port)
 }
 bootstrap()
